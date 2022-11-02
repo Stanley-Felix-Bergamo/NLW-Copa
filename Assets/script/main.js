548dd630e8cc4055646d668adc378ec172a875c1
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2022-11-02 12:49:04 -0300

    Commit

--- Assets/script/main.js
+++ Assets/script/main.js
@@ -22,9 +22,22 @@ document.querySelector('#app').innerHTML = `
        <img src="./Assets/img/logo.svg" alt="Logo da NLW">
    </header>
    <main id="cards">
-        ${createCard('24/11', 'quinta', createGame('brasil', '16:00', 'serbia'))}   
-        ${createCard('28/11', 'segunda', createGame('switzerland', '13:00', 'brasil') +
-          createGame('portugal', '16:00', 'uruguay'))}
-        ${createCard('02/12', 'sexta', createGame('brasil', '16:00', 'cameroon'))}
+        ${createCard('24/11', 'quinta',
+        createGame('brasil', '16:00', 'serbia') +
+        createGame('portugal', '13:00', 'uruguay') +
+        createGame('colombia', '13:00', 'japan')
+)}  
+
+        ${createCard('28/11', 'segunda',
+          createGame('south korea', '16:00', 'ghana') +
+          createGame('switzerland', '13:00', 'brasil') +
+          createGame('portugal', '13:00', 'uruguay')
+        )}
+
+        ${createCard('02/12', 'sexta', 
+        createGame('brasil', '16:00', 'cameroon') +
+        createGame('british columbia', '13:00', 'india') +
+        createGame('comoros', '20:00', 'jordan') 
+        )}
    </main>
 `;
